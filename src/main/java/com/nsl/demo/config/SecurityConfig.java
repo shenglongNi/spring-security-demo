65a094cabca4c2cf82cb8f14583c6907647fcb44
shenglongNi <shenglongni@163.com>
2019-06-25 15:20:41 +0800

    简单的用户登录认证，In memory

--- src/main/java/com/nsl/demo/config/SecurityConfig.java
+++ src/main/java/com/nsl/demo/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.nsl.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -33,4 +40,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .logout();
 
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(passwordEncoderBean());
+
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("nsl").passwordEncoder(s -> new BCryptPasswordEncoder().encode("111111")).roles("ADMIN").build());
+        manager.createUser(User.withUsername("zl").password("222222").roles("USER", "ADMIN").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoderBean() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
